30bf8c75d800cfc5a7b7aed32d04aab3cef60fd6
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2023-12-23 15:48:46 +0700

    Lab3 complete

--- Lab3/src/OddEven.java
+++ Lab3/src/OddEven.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 public class OddEven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int odd = 0;
-        int even = 0;
+        int odd = 0, even = 0;
         boolean check = true;
         while(check){
             int number = sc.nextInt();
             if (number == -1){
                 break;
             } else if((number % 2) == 0){
-                even += 1;
+                even++;
             } else if((number % 2) == 1){
-                odd += 1;
+                odd++;
             }
         }System.out.println("Odd number = " + odd + " and Even number = " + even);
     }
